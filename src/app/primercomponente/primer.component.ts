004879bde2191a03f7ec73735a03d5cef0e9409e
vn.dmateos <dmateosca@viewnext.com>
2020-03-31 18:01:35 +0200

    inicio

---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-hello-world',
+    templateUrl : './primer.component.html',
+    styleUrls: ['./primer.component.css']
+
+})
+// tslint:disable-next-line:component-class-suffix
+export class HelloWord{
+
+}
+
